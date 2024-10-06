7ad51affcc6108cfd7528c985be5ef0b8c89fcbc
SHUBHAM KUMAR <117589831+SHUBHAMKUMAR45@users.noreply.github.com>
2024-10-06 19:43:18 +0530

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,141 @@
+import React from "react";
+import "../Styles/Footer.css";
+import SubscribeNewsletter from "./SubscribeNewsletter";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div className="footer-section">
+      <div className="footer-container">
+        <div className="ft-info">
+          <div className="ft-info-p1">
+            <p className="ft-title">
+              Health <span className="ft-sign">+</span>
+            </p>
+            <p className="ft-description">
+              Talk to online doctors and get medical advice, online
+              prescriptions, refills and medical notes within minutes. On-demand
+              healthcare services at your fingertips.
+            </p>
+          </div>
+
+          <SubscribeNewsletter />
+        </div>
+
+        <div className="ft-list">
+          <p className="ft-list-title">Services</p>
+          <ul className="ft-list-items">
+            <li>
+              <a href="#services">Emergency Care</a>
+            </li>
+            <li>
+              <a href="#services">Heart Disease</a>
+            </li>
+            <li>
+              <a href="#services">Dental Care</a>
+            </li>
+            <li>
+              <a href="#services">Prescription</a>
+            </li>
+            <li>
+              <a href="#services">Insights for doctors</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="ft-list">
+          <p className="ft-list-title">Legal</p>
+          <ul className="ft-list-items">
+            <li>
+              <Link to={"/legal"}>General Info</Link>
+            </li>
+            <li>
+              <Link to={"/legal"}>Privacy Policy</Link>
+            </li>
+            <li>
+              <Link to={"/legal"}>Terms of Services</Link>
+            </li>
+            <li>
+              <Link to={"/legal"}>Consultations</Link>
+            </li>
+            <li>
+              <Link to={"/legal"}>How it Works</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="ft-list" id="contact">
+          <p className="ft-list-title">Talk To Us</p>
+          <ul className="ft-list-items">
+            <li>
+              <a href="mailto:support@healthplus.com">support@healthplus.com</a>
+            </li>
+            <li>
+              <a href="mailto:appointment@healthplus.com">
+                appointment@healthplus.com
+              </a>
+            </li>
+            <li>
+              <a href="tel:+022 5454 5252">+022 5454 5252</a>
+            </li>
+            <li>
+              <a href="tel:+022 2326 6232">+022 2326 6232</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="ft-copyright">
+        <p>© 2013-2024 Health+. All rights reserved.</p>
+
+        <ul className="ft-social-links">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/-shubham-kumar/"
+              title="LinkedIn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 448 512"
+              >
+                <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z" />
+              </svg>
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://facebook.com/"
+              title="FaceBook"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 320 512"
+              >
+                <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
+              </svg>
+            </a>
+          </li>
+
+          <li>
+            <a 
+              href="https://www.instagram.com/i_am__shubham_kumar/"
+              title="LinkedIn"
+              target="_blank"
+              rel="noopener noreferrer">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11.984 16.815c2.596 0 4.706-2.111 4.706-4.707 0-1.409-.623-2.674-1.606-3.538-.346-.303-.735-.556-1.158-.748-.593-.27-1.249-.421-1.941-.421s-1.349.151-1.941.421c-.424.194-.814.447-1.158.749-.985.864-1.608 2.129-1.608 3.538 0 2.595 2.112 4.706 4.706 4.706zm.016-8.184c1.921 0 3.479 1.557 3.479 3.478 0 1.921-1.558 3.479-3.479 3.479s-3.479-1.557-3.479-3.479c0-1.921 1.558-3.478 3.479-3.478zm5.223.369h6.777v10.278c0 2.608-2.114 4.722-4.722 4.722h-14.493c-2.608 0-4.785-2.114-4.785-4.722v-10.278h6.747c-.544.913-.872 1.969-.872 3.109 0 3.374 2.735 6.109 6.109 6.109s6.109-2.735 6.109-6.109c.001-1.14-.327-2.196-.87-3.109zm2.055-9h-12.278v5h-1v-5h-1v5h-1v-4.923c-.346.057-.682.143-1 .27v4.653h-1v-4.102c-1.202.857-2 2.246-2 3.824v3.278h7.473c1.167-1.282 2.798-2 4.511-2 1.722 0 3.351.725 4.511 2h7.505v-3.278c0-2.608-2.114-4.722-4.722-4.722zm2.722 5.265c0 .406-.333.735-.745.735h-2.511c-.411 0-.744-.329-.744-.735v-2.53c0-.406.333-.735.744-.735h2.511c.412 0 .745.329.745.735v2.53z"/></svg>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
